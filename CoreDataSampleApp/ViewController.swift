7c21a6c57a761f4bdb6d2a86e5491334e4a7c5cf
NirmalKumar <imdinesh.58@gmail.com>
2018-02-26 13:59:33 +0530

    Dynamic TEXTSVIEW
    Dynamic TEXTSVIEW

---
+++ CoreDataSampleApp/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  CoreDataSampleApp
+//
+//  Created by Apple-1 on 26/02/18.
+//  Copyright © 2018 Apple-1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UITextViewDelegate {
+
+    @IBOutlet weak var textView: UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    func textViewDidChange(_ textView: UITextView) {
+        let fixedWidth = textView.frame.size.width
+        textView.sizeThatFits(CGSize(width: fixedWidth, height: CGFloat.greatestFiniteMagnitude))
+        let newSize = textView.sizeThatFits(CGSize(width: fixedWidth, height: CGFloat.greatestFiniteMagnitude))
+        var newFrame = textView.frame
+        newFrame.size = CGSize(width: max(newSize.width, fixedWidth), height: newSize.height)
+        textView.frame = newFrame
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
